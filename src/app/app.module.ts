03a351e3a00f76d7739a20b5a7585bff3ec0f7d9
dicks6n <dikkymccria@gmail.com>
2019-07-15 00:59:28 -0700

    initialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes} from '@angular/router'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GitBarComponent } from './components/git-bar/git-bar.component';
+import { UsersComponent } from './components/users/users.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
+import { RepositoriesComponent } from './components/repositories/repositories.component';
+import { FinalPipe } from './custompipe/final.pipe';
+
+const routes: Routes = [
+  {path: 'users', component: UsersComponent},
+  {path: 'repository', component: RepositoriesComponent},
+  {path: '', redirectTo: '/users', pathMatch: 'full'},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitBarComponent,
+    UsersComponent,
+    SearchFormComponent,
+    RepositoriesComponent,
+    FinalPipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
